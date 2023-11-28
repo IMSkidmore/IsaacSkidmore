ba840d8e8310bde85a8209b1445787b34ce663b6
Isaac Skidmore <isaac.skidmore@zenith-semi.com>
2023-11-28 10:15:22 -0700

    general updates, some stuff is broken but general improvements all around

--- script.js
+++ script.js
@@ -1,13 +1,65 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Add functionality to show the default tab
-    showTab(window.location.hash || '#about');
+    const body = document.body;
 
-    // Add event listeners for navigation
+    // Responsive Navigation
+    const navbar = document.querySelector('.navbar');
+    const toggleBtn = document.createElement('button');
+    toggleBtn.innerHTML = '☰';
+    toggleBtn.classList.add('navbar-toggle');
+    toggleBtn.addEventListener('click', function () {
+        navbar.classList.toggle('active');
+    });
+    navbar.appendChild(toggleBtn);
+
+    // Smooth Scrolling
+    const scrollLinks = document.querySelectorAll('.navbar a[href^="#"]');
+    scrollLinks.forEach(link => {
+        link.addEventListener('click', function (event) {
+            event.preventDefault();
+
+            const targetId = this.getAttribute('href');
+            const targetElement = document.querySelector(targetId);
+
+            if (targetElement) {
+                window.scrollTo({
+                    top: targetElement.offsetTop - navbar.offsetHeight,
+                    behavior: 'smooth'
+                });
+            }
+
+            // Close the navigation menu on mobile
+            navbar.classList.remove('active');
+        });
+    });
+
+    // Toggle Dark Mode with the switch
+    const darkModeToggle = document.getElementById('dark-mode-toggle');
+
+    darkModeToggle.addEventListener('change', function () {
+        body.classList.toggle('dark-mode', this.checked);
+        // Save user preference in localStorage
+        localStorage.setItem('darkMode', this.checked);
+    });
+
+    // Check for dark mode preference in localStorage
+    const savedDarkMode = localStorage.getItem('darkMode');
+    if (savedDarkMode) {
+        body.classList.toggle('dark-mode', savedDarkMode === 'true');
+        darkModeToggle.checked = savedDarkMode === 'true';
+    }
+
+    // Tabs functionality
     const tabs = document.querySelectorAll('.navbar a');
     tabs.forEach(tab => {
         tab.addEventListener('click', function (event) {
             const targetTab = this.getAttribute('href');
             showTab(targetTab);
+
+            // Prevent default behavior
+            event.preventDefault();
+
+            // Close the navigation menu on mobile
+            navbar.classList.remove('active');
         });
     });
 });
