4964ca7df51b133c31ccd3685760e6d221de73ad
Isaac Skidmore <isaac.skidmore@zenith-semi.com>
2023-11-30 14:47:55 -0700

    Last update before moving to react

--- script.js
+++ script.js
@@ -1,77 +1,40 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const body = document.body;
+document.addEventListener("DOMContentLoaded", function () {
+    const canvasContainer = document.createElement("div");
+    canvasContainer.className = "canvas-container";
+    document.body.appendChild(canvasContainer);
 
-    // Responsive Navigation
-    const navbar = document.querySelector('.navbar');
-    const toggleBtn = document.createElement('button');
-    toggleBtn.innerHTML = '☰';
-    toggleBtn.classList.add('navbar-toggle');
-    toggleBtn.addEventListener('click', function () {
-        navbar.classList.toggle('active');
-    });
-    navbar.appendChild(toggleBtn);
+    const canvas = document.createElement("canvas");
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    canvasContainer.appendChild(canvas);
 
-    // Smooth Scrolling
-    const scrollLinks = document.querySelectorAll('.navbar a[href^="#"]');
-    scrollLinks.forEach(link => {
-        link.addEventListener('click', function (event) {
-            event.preventDefault();
+    const ctx = canvas.getContext("2d");
 
-            const targetId = this.getAttribute('href');
-            const targetElement = document.querySelector(targetId);
+    let isDrawing = false;
 
-            if (targetElement) {
-                window.scrollTo({
-                    top: targetElement.offsetTop - navbar.offsetHeight,
-                    behavior: 'smooth'
-                });
-            }
-
-            // Close the navigation menu on mobile
-            navbar.classList.remove('active');
-        });
+    document.addEventListener("mousemove", function (e) {
+        if (isDrawing) {
+            drawLine(e.clientX, e.clientY);
+        }
     });
 
-    // Toggle Dark Mode with the switch
-    const darkModeToggle = document.getElementById('dark-mode-toggle');
-
-    darkModeToggle.addEventListener('change', function () {
-        body.classList.toggle('dark-mode', this.checked);
-        // Save user preference in localStorage
-        localStorage.setItem('darkMode', this.checked);
+    document.addEventListener("mousedown", function (e) {
+        isDrawing = true;
+        ctx.beginPath();
+        drawLine(e.clientX, e.clientY);
     });
 
-    // Check for dark mode preference in localStorage
-    const savedDarkMode = localStorage.getItem('darkMode');
-    if (savedDarkMode) {
-        body.classList.toggle('dark-mode', savedDarkMode === 'true');
-        darkModeToggle.checked = savedDarkMode === 'true';
+    document.addEventListener("mouseup", function () {
+        isDrawing = false;
+    });
+
+    function drawLine(x, y) {
+        ctx.lineWidth = 2; // Adjust the thickness of the line
+        ctx.lineCap = "round";
+        ctx.strokeStyle = "rgb(0, 255, 64)";
+        ctx.lineTo(x, y);
+        ctx.stroke();
+        ctx.beginPath();
+        ctx.moveTo(x, y);
     }
-
-    // Tabs functionality
-    const tabs = document.querySelectorAll('.navbar a');
-    tabs.forEach(tab => {
-        tab.addEventListener('click', function (event) {
-            const targetTab = this.getAttribute('href');
-            showTab(targetTab);
-
-            // Prevent default behavior
-            event.preventDefault();
-
-            // Close the navigation menu on mobile
-            navbar.classList.remove('active');
-        });
-    });
 });
-
-function showTab(tabId) {
-    const allTabs = document.querySelectorAll('.tab-content');
-    allTabs.forEach(tab => {
-        tab.style.display = 'none';
-    });
-
-    const targetTab = document.querySelector(tabId);
-    if (targetTab) {
-        targetTab.style.display = 'block';
-    }
-}
